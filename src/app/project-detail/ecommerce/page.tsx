d03e090972d7b2d627ce28ddcad4e08260f9ba9a
Mukesh Kumar <thappamkkumar@gmail.com>
2026-04-02 16:44:42 +0530

    remove unwanted files

--- src/app/project-detail/ecommerce/page.tsx
+++ src/app/project-detail/ecommerce/page.tsx
@@ -1,33 +1,13 @@
  import Header from "@/components/project-detail/Header";
-import HeroSection from "@/components/project-detail/HeroSection";
-import TechStack from "@/components/project-detail/TechStack";
-import Overview from "@/components/project-detail/overview/ecommerse/Overview";
-
-import Feature from "@/components/project-detail/feature/Feature";
-import ScrollToTopButton from "@/components/common/ScrollToTopButton";
-
-
-import {
-  heroProps, 
-  techStackProps,
-  featureProps,
-} from "@/lib/project-detail-data/ecommerse/data";
+ 
+ 
 
 export default function Ecommerce() {
   return (
     <>
-			<Header data={featureProps}/>
+			<Header  />
 			<main>
-				<HeroSection {...heroProps} />
-				<div className="px-6">
-					<Overview />
-					<TechStack stack={techStackProps.stack} />
-					
-					 <Feature data={featureProps} />
-
-				</div>
-				
-				<ScrollToTopButton />
+				 
 				 
 			</main>
     </>
--- src/app/project-detail/multi-vendor-ecommerce/page.tsx
+++ src/app/project-detail/multi-vendor-ecommerce/page.tsx
@@ -1,33 +1,12 @@
 
 import Header from "@/components/project-detail/Header";
-import HeroSection from "@/components/project-detail/HeroSection";
-import TechStack from "@/components/project-detail/TechStack";
-import Overview from "@/components/project-detail/overview/multi-vendor-ecommerse/Overview";
-import Feature from "@/components/project-detail/feature/Feature";
- 
-import ScrollToTopButton from "@/components/common/ScrollToTopButton";
-
-import {
-  heroProps, 
-  techStackProps,
-  featureProps,
-} from "@/lib/project-detail-data/multi-vendor-ecommerse/data";
-
+  
 export default function MultiVendorEcommerce() {
   return (
     <>
-			<Header data={featureProps}/>
+			<Header />
 			<main>
-				<HeroSection {...heroProps} />
-				<div className="px-6">
-					<Overview />
-					<TechStack stack={techStackProps.stack} />
-					
-					<Feature data={featureProps} />
-
-				</div>
-				
-				<ScrollToTopButton />
+				 
 				 
 			</main>
     </>
